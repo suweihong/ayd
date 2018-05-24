c60221dd7eb6795e32e25e2c04002bf5b9e272d9
huyue <1352135841@qq.com>
2018-05-24 11:13:32 +0800

    1

---
+++ resources/views/store/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.layout')
+
+@section('title','商家管理')
+
+@section('content')
+	<div class="row">
+		<div class="col-xs-12 in_box">
+			<div class="alert" role="alert">
+				<span class="in_title">商家管理</span>
+			</div>
+			<ul id="store_menu1" class="col-xs-12">
+				<li class="active"><a href="" data-toggle="tab">基础信息</a></li>
+				<li><a href="" data-toggle="tab">销售</a></li>
+				<li><a href="" data-toggle="tab">订单</a></li>
+				<li><a href="" data-toggle="tab">评价</a></li>
+			</ul>
+		</div>
+	</div>		
+@stop
