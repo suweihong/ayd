3f73281debe1f9fcd869e35ae296b09e82cb79ce
huyue <1352135841@qq.com>
2018-05-24 17:26:18 +0800

    1

--- resources/views/store/_first.blade.php
+++ resources/views/store/_first.blade.php
@@ -4,12 +4,19 @@
 @section('title','商家管理')
 
 @section('content')
-	<div>
-		<a href="{{route('stores.edit',1)}}">基础信息</a>
-		<a href="{{route('items.index')}}">销售</a>
-		<a href="{{route('orders.index')}}">订单</a>
-		<a href="{{route('estimates.index')}}">评价</a>
-	 </div>
+	<div class="row">
+		<div class="col-xs-12 in_box">
+			<div class="alert" role="alert">
+				<span class="in_title">商家管理</span>
+			</div>
+			<ul id="store_menu1" class="col-xs-12">
+				<li class="active"><a href="{{route('stores.edit',1)}}">基础信息</a></li>
+				<li><a href="{{route('items.index')}}">销售</a></li>
+				<li><a href="{{route('orders.index')}}">订单</a></li>
+				<li><a href="{{route('estimates.index')}}">评价</a></li>
+			</ul>
+		</div>
+	</div>
 	<div class="row col-sm-12">
 		@yield('substance')
 	</div>
