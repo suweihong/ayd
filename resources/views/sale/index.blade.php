74bb3070c089a467ade25e8f3057352ec9297cfe
huyue <1352135841@qq.com>
2018-05-25 18:19:28 +0800

    1

--- resources/views/sale/index.blade.php
+++ resources/views/sale/index.blade.php
@@ -1,4 +1,34 @@
 @extends('store._third')
 	@section('subject')
-	<h1>销售</h1>
+	<!-- <h1>销售</h1> -->
+	<ul class="col-sm-12" id="salePlace">
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn-info">场地1</li>
+		<li class="btn btn_btn">新增</li>
+	</ul>
+	<div class="row" id="storeTime">
+		<div class="col-xs-12">
+		<label>
+			<span>营业时间:　</span>
+		</label>
+			<input type="time">
+		<label>至</label>
+		<input type="time">	
+		</div>
+	</div>
+	<div class="btn btn-info updatanum">更新销售数据</div>
 @stop
