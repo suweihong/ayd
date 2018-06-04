795063d106a3e7c58435bde3238a40ffc98c5e2f
huyue <1352135841@qq.com>
2018-06-04 16:39:20 +0800

    1

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -4,7 +4,10 @@
 @section('title','订单管理')
 
 @section('content')
-
-	@include('orders._orders')
-	<h1>所有订单</h1>
+	<!-- <h1>所有订单</h1> -->
+	<div class="row in_box col-xs-12">
+		<div class="alert">
+			<span class="in_title">订单查询</span>
+		</div>
+	</div>
 @stop
