74ada0ab4cf6d16783793ef8d757afcb25f117f8
huyue <1352135841@qq.com>
2018-06-14 10:47:47 +0800

    1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,62 +4,35 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>登录</title>
-
-<link href="css/admin/bootstrap.min.css" rel="stylesheet">
-<link href="css/admin/datepicker3.css" rel="stylesheet">
 <link href="css/admin/styles.css" rel="stylesheet">
-
+<script src="js/admin/jquery-1.11.1.min.js"></script>
 <!--[if lt IE 9]>
 <script src="js/admin/html5shiv.js"></script>
 <script src="js/admin/respond.min.js"></script>
 <![endif]-->
-
 </head>
-
-<body>
-	
+<body>	
 	<div class="row">
-@include('_messages')
-
-
-
+	@include('_messages')
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 			<div class="login-panel panel panel-default">
 				<div class="panel-heading">奥运动管理员平台</div>
 				<div class="panel-body">
 					<form role="form" action="/login" method="post">
 						{{ csrf_field() }}
-
 						<fieldset>
-							<!-- <div class="form-group">
-								<input class="form-control" placeholder="E-mail" name="email" type="email" autofocus="">
-							</div> -->
 							<div class="form-group">
 								<input class="form-control" placeholder="Password" name="password" type="password" value="" >
 							</div>
 							<div class="checkbox">
-								<!-- <label>
-									<input name="remember" type="checkbox" value="Remember Me">Remember Me
-								</label> -->
 							</div>
-							<!-- <a href="" class="btn btn-primary">登录</a> -->
 							<button class="btn btn-primary"  type="submit" >登录</button>
 						</fieldset>
 					</form>
 				</div>
 			</div>
-		</div><!-- /.col-->
-	</div><!-- /.row -->	
-	
-		
-
-	<script src="js/admin/jquery-1.11.1.min.js"></script>
-	<script src="js/admin/bootstrap.min.js"></script>
-	<script src="js/admin/chart.min.js"></script>
-	<!-- <script src="js/admin/chart-data.js"></script> -->
-	<script src="js/admin/easypiechart.js"></script>
-	<!-- <script src="js/admin/easypiechart-data.js"></script> -->
-	<script src="js/admin/bootstrap-datepicker.js"></script>
+		</div>
+	</div>
 	<script>
 		!function ($) {
 			$(document).on("click","ul.nav li.parent > a > span.icon", function(){		  
@@ -67,7 +40,6 @@
 			}); 
 			$(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
 		}(window.jQuery);
-
 		$(window).on('resize', function () {
 		  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
 		})
@@ -76,5 +48,4 @@
 		})
 	</script>	
 </body>
-
 </html>
